da0ff2edec84b0da0f54d012500a8e1b4d115a2d
JM_DEV <kyungheeyj45@ajou.ac.kr>
2025-03-28 09:54:31 +0900

    feat: 연동

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ECR_IMAGE="515966524744.dkr.ecr.ap-northeast-2.amazonaws.com/jm-story-be:latest"
+CONTAINER_NAME="fastapi-backend"
+
+# 기존 컨테이너 정리
+docker rm -f $CONTAINER_NAME || true
+
+# 최신 이미지 가져오기
+aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <account_id>.dkr.ecr.<region>.amazonaws.com
+docker pull $ECR_IMAGE
+
+# 컨테이너 실행
+docker run -d \
+  --name $CONTAINER_NAME \
+  -p 8000:8000 \
+  $ECR_IMAGE
